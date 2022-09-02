06b08cccb474b82e0b3b33e992ab6675320b8a6d
neicore <neicoreadams@gmail.com>
2022-09-03 00:00:41 +0300

    :sparkles: add player

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = outerWidth
-canvas.height = outerHeight
+canvas.width = innerWidth
+canvas.height = innerHeight
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+}
+
+const centerX = canvas.width / 2
+const centerY = canvas.height / 2
+
+const player = new Player(centerX, centerY, 30, 'blue')
+player.draw()

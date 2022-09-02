b0449b8a8472d5313432c1ab6c538ab1cf4314f5
neicore <neicoreadams@gmail.com>
2022-09-03 02:20:09 +0300

    :sparkles: clear projectiles that hit the edge of the screen

--- js/index.js
+++ js/index.js
@@ -107,8 +107,19 @@ const animate = () => {
   c.clearRect(0, 0, canvas.width, canvas.height)
   player.draw()
 
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update()
+
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(index, 1)
+      }, 0)
+    }
   })
 
   enemies.forEach((enemy, index) => {

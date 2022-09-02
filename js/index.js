97425198baa43eedcd9dd40c0ae206d01f9cdf7b
neicore <neicoreadams@gmail.com>
2022-09-02 23:39:50 +0300

    :tada: project setup

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = outerWidth
+canvas.height = outerHeight
+
+c.fillRect(0, 0, canvas.width, canvas.height)

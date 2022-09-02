bee6297f2c7e2d53156173f1670101151c38f0a8
neicore <neicoreadams@gmail.com>
2022-09-03 00:48:46 +0300

    :sparkles: add ability to shoot projectiles

--- js/index.js
+++ js/index.js
@@ -22,8 +22,52 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
 const centerX = canvas.width / 2
 const centerY = canvas.height / 2
 
 const player = new Player(centerX, centerY, 30, 'blue')
-player.draw()
+
+const projectiles = []
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  player.draw()
+  projectiles.forEach((projectile) => {
+    projectile.update()
+  })
+}
+
+addEventListener('click', (e) => {
+  const angle = Math.atan2(e.clientY - centerY, e.clientX - centerX)
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  }
+  projectiles.push(new Projectile(centerX, centerY, 5, 'red', velocity))
+})
+
+animate()

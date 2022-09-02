991eb7b1b85b4a7e6bd3cad58eb9f2218b1701cb
neicore <neicoreadams@gmail.com>
2022-09-03 01:30:27 +0300

    :sparkles: add ability to create enemies

--- js/index.js
+++ js/index.js
@@ -45,19 +45,72 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
 const centerX = canvas.width / 2
 const centerY = canvas.height / 2
 
 const player = new Player(centerX, centerY, 30, 'blue')
-
 const projectiles = []
+const enemies = []
+
+const spawnEnemies = () => {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 5) + 5
+    let x
+    let y
+
+    if (Math.random() <= 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+
+    const color = 'green'
+    const angle = Math.atan2(centerY - y, centerX - x)
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    }
+    enemies.push(new Enemy(x, y, radius, color, velocity))
+  }, 1000)
+}
 
 const animate = () => {
   requestAnimationFrame(animate)
+
   c.clearRect(0, 0, canvas.width, canvas.height)
   player.draw()
+
   projectiles.forEach((projectile) => {
     projectile.update()
+  })
+
+  enemies.forEach((enemy) => {
+    enemy.update()
   })
 }
 
@@ -71,3 +124,4 @@ addEventListener('click', (e) => {
 })
 
 animate()
+spawnEnemies()

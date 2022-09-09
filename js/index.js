b4770a7be27097c05a3089d4c2c26570c5770822
neicore <neicoreadams@gmail.com>
2022-09-09 21:47:37 +0300

    :sparkles: create particle explosion on hit

--- js/index.js
+++ js/index.js
@@ -66,12 +66,45 @@ class Enemy {
   }
 }
 
+const friction = 0.98
+
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+    this.alpha = 1
+  }
+
+  draw() {
+    c.save()
+    c.globalAlpha = this.alpha
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.restore()
+  }
+
+  update() {
+    this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.alpha -= 0.01
+  }
+}
+
 const centerX = canvas.width / 2
 const centerY = canvas.height / 2
 
 const player = new Player(centerX, centerY, 20, 'white')
 const projectiles = []
 const enemies = []
+const particles = []
 
 const spawnEnemies = () => {
   setInterval(() => {
@@ -108,6 +141,14 @@ const animate = () => {
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.draw()
 
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1)
+    } else {
+      particle.update()
+    }
+  })
+
   projectiles.forEach((projectile, index) => {
     projectile.update()
 
@@ -134,19 +175,34 @@ const animate = () => {
       cancelAnimationFrame(animationId)
     }
 
+    // Enemy vs projectile collision
     projectiles.forEach((projectile, projectileIndex) => {
-      // Enemy vs projectile collision
       const distance = Math.hypot(
         projectile.x - enemy.x,
         projectile.y - enemy.y
       )
 
       if (distance - enemy.radius - projectile.radius < 1) {
-        console.log(enemy.radius)
+        // particle explosion
+        for (let i = 0; i <= enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 5),
+                y: (Math.random() - 0.5) * (Math.random() * 5),
+              }
+            )
+          )
+        }
+
         if (enemy.radius - 10 > 5) {
           gsap.to(enemy, {
-           radius: enemy.radius - 10
-         })
+            radius: enemy.radius - 10,
+          })
 
           setTimeout(() => {
             projectiles.splice(projectileIndex, 1)

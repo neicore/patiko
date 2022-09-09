abe1406efc7de0c77fb40b59acdca189ff6446b3
neicore <neicoreadams@gmail.com>
2022-09-09 23:17:36 +0300

    :recycle: code refactor

--- js/index.js
+++ js/index.js
@@ -10,100 +10,6 @@ const gameOverUI = document.getElementById('game_over')
 const finalScoreUI = document.getElementById('final_score')
 const restartButton = document.getElementById('restart')
 
-class Player {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-  }
-}
-
-class Projectile {
-  constructor(x, y, radius, color, velocity) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.velocity = velocity
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-  }
-
-  update() {
-    this.draw()
-    this.x = this.x + this.velocity.x
-    this.y = this.y + this.velocity.y
-  }
-}
-
-class Enemy {
-  constructor(x, y, radius, color, velocity) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.velocity = velocity
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-  }
-
-  update() {
-    this.draw()
-    this.x = this.x + this.velocity.x
-    this.y = this.y + this.velocity.y
-  }
-}
-
-const friction = 0.98
-
-class Particle {
-  constructor(x, y, radius, color, velocity) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.velocity = velocity
-    this.alpha = 1
-  }
-
-  draw() {
-    c.save()
-    c.globalAlpha = this.alpha
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.restore()
-  }
-
-  update() {
-    this.draw()
-    this.velocity.x *= friction
-    this.velocity.y *= friction
-    this.x = this.x + this.velocity.x
-    this.y = this.y + this.velocity.y
-    this.alpha -= 0.01
-  }
-}
-
 const centerX = canvas.width / 2
 const centerY = canvas.height / 2
 
@@ -260,10 +166,5 @@ animate()
 spawnEnemies()
 
 restartButton.addEventListener('click', () => {
-  newGame()
-  animate()
-  spawnEnemies()
-  gameOverUI.style.display = 'none'
-  scoreParagraph.style.display = 'block'
-  scoreUI.textContent = score
+  location.reload()
 })
---
+++ js/objects.js
@@ -0,0 +1,92 @@
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+    this.alpha = 1
+  }
+
+  draw() {
+    c.save()
+    c.globalAlpha = this.alpha
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.restore()
+  }
+
+  update() {
+    const friction = 0.98
+    this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.alpha -= 0.01
+  }
+}
